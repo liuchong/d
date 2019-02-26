43b71025470ec5198acd130e4837ccba64874bef
Liu Chong <mail@liuchong.io>
2019-02-26 19:48:29 +0800

    feat: add HTTP range request support
    Implement Content-Range header support for partial content delivery.
    Enables video streaming and download resume functionality.

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,31 @@
 use mime_guess::get_mime_type_str as guess_mime_type_str;
+use regex::Regex;
 use std::path::Path;
+
+#[derive(Default)]
+pub struct Range(pub String, pub u64, pub Option<u64>);
+
+pub fn parse_range(range: &str) -> Option<Range> {
+    // Range Syntax, 3, 4 not supported for now
+    // 1. Range: <unit>=<range-start>-
+    // 2. Range: <unit>=<range-start>-<range-end>
+    // 3. Range: <unit>=<range-start>-<range-end>, <range-start>-<range-end>
+    // 4. Range: <unit>=<range-start>-<range-end>, <range-start>-<range-end>, <range-start>-<range-end>
+
+    let re = Regex::new(r"(.*)=(\d+)-(\d+)?").unwrap(); // will success
+    match re.captures(range) {
+        Some(cap) => {
+            let unit = cap.get(1).unwrap().as_str().to_string();
+            let start = cap.get(2).unwrap().as_str().parse::<u64>().unwrap();
+            let end = match cap.get(3) {
+                Some(m) => Some(m.as_str().parse::<u64>().unwrap()),
+                _ => None,
+            };
+            Some(Range(unit, start, end))
+        }
+        _ => None,
+    }
+}
 
 pub fn get_mime_type_str(filename: &str) -> &str {
     let ext = Path::new(&filename)
@@ -7,6 +33,5 @@ pub fn get_mime_type_str(filename: &str) -> &str {
         .unwrap_or_default()
         .to_str()
         .unwrap_or_default();
-
     guess_mime_type_str(ext).unwrap_or("application/octet-stream")
 }
